cb39d963a66174b4133fcaff34f2f8485fecfc9a
will <349071347@qq.com>
2019-04-18 19:07:41 +0800

    update pom

--- src/main/java/locals/fescar/test/demo/DemoApplication.java
+++ src/main/java/locals/fescar/test/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package locals.fescar.test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author will
+ */
 @SpringBootApplication
 public class DemoApplication {
 
