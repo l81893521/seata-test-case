ed6d7e0203bf55dc53d462a1839346ecd318ca8f
will <349071347@qq.com>
2019-04-19 17:51:28 +0800

    init project

--- springboot-dubbo/src/main/java/com/seata/test/springbootdubbo/SpringbootDubboApplication.java
+++ springboot-dubbo/src/main/java/com/seata/test/springbootdubbo/SpringbootDubboApplication.java
@@ -3,6 +3,9 @@ package com.seata.test.springbootdubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author will
+ */
 @SpringBootApplication
 public class SpringbootDubboApplication {
 
