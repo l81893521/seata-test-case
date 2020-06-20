b6513f2f284d59d762526a63237558a1b920d5c4
Jiawei.Zhang <jiawei.zhang@yatsenglobal.com>
2020-06-20 10:50:28 +0800

    at-dubbo-spring-mybatis增加storage模块

---
+++ at-dubbo-springboot-mybatis/storage-service/src/main/java/com/seata/config/DataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.seata.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+  * @author will.zjw
+  * @date 2019-12-24 15:19
+  *            佛主保佑,不要出BUG
+  *                 _ooOoo_
+  *                o8888888o
+  *                88" . "88
+  *                (| -_- |)
+  *                O\  =  /O
+  *             ____/`---'\____
+  *           .'  \\|     |//  `.
+  *          /  \\|||  :  |||//  \
+  *         /  _||||| -:- |||||-  \
+  *         |   | \\\  -  /// |   |
+  *         | \_|  ''\---/''  |   |
+  *          \  .-\__  `-`  ___/-. /
+  *        ___`. .'  /--.--\  `. . __
+  *      ."" '<  `.___\_<|>_/___.'  >'"".
+  *    | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+  *    \  \ `-.   \_ __\ /__ _/   .-` /  /
+  * ======`-.____`-.___\_____/___.-`____.-'======
+  *                 `=---='
+  */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+}
