dd23cd9dcd47574ecefb291f6416ef8f37d8fada
Jiawei.Zhang <jiawei.zhang@yatsenglobal.com>
2020-07-14 10:52:30 +0800

    去除多余注释

--- at-dubbo-springboot-mybatis/account-service/src/main/java/com/seata/Application.java
+++ at-dubbo-springboot-mybatis/account-service/src/main/java/com/seata/Application.java
@@ -7,25 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
   * @author will.zjw
   * @date 2019-12-16 17:13
-  *            佛主保佑,不要出BUG
-  *                 _ooOoo_
-  *                o8888888o
-  *                88" . "88
-  *                (| -_- |)
-  *                O\  =  /O
-  *             ____/`---'\____
-  *           .'  \\|     |//  `.
-  *          /  \\|||  :  |||//  \
-  *         /  _||||| -:- |||||-  \
-  *         |   | \\\  -  /// |   |
-  *         | \_|  ''\---/''  |   |
-  *          \  .-\__  `-`  ___/-. /
-  *        ___`. .'  /--.--\  `. . __
-  *      ."" '<  `.___\_<|>_/___.'  >'"".
-  *    | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-  *    \  \ `-.   \_ __\ /__ _/   .-` /  /
-  * ======`-.____`-.___\_____/___.-`____.-'======
-  *                 `=---='
   */
 @SpringBootApplication(scanBasePackages = "com.seata", exclude = {DataSourceAutoConfiguration.class})
 public class Application {
